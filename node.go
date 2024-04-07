5161ee834be4b51a86528c95912581feeae32842
Alexander Latour <alexlatour@gmail.com>
2024-04-07 15:18:05 -0400

    fixed bug in config, WIP: adding support for expired nodes

--- node.go
+++ node.go
@@ -1,6 +1,30 @@
 package main
 
+import "time"
+
 type node struct {
-	key   string
-	value string
+	key        string
+	value      string
+	expires_at time.Time
+}
+
+func (n node) isExpired() bool {
+	if n.expires_at.IsZero() {
+		return false
+	}
+
+	return time.Now().UnixMicro() < n.expires_at.UnixMicro()
+}
+
+func (n *node) setExpiresAt(time time.Time) *node {
+	n.expires_at = time
+	return n
+}
+
+func NewNode(key string, value string) *node {
+	n := node{}
+	n.key = key
+	n.value = value
+
+	return &n
 }
